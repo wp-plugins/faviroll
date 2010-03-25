f917058961e2234efbf2c4bbbef8413d1db6b050
UnderWordPressure <UnderWordPressure@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-25 20:25:56 +0000

    Settings Link verlagert
    git-svn-id: https://plugins.svn.wordpress.org/faviroll/trunk@221693 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- faviroll.php
+++ faviroll.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: FAVIROLL - FAVIcons for blogROLL
 Plugin URI: http://www.grobator.de/wordpress-stuff/plugins/faviroll
-Description: Locally caches all favicon.ico in PNG format and use this into the blogroll. Native ICO Images are not supported from all browsers/operating systems. <strong><a href="link-manager.php?page=faviroll.php">For Settings jump to: Links &raquo; Faviroll</a></strong>
+Description: Locally caches all favicon.ico in PNG format and use this into the blogroll. Native ICO Images are not supported from all browsers/operating systems.
 Author: grobator
 Version:  0.4.7
 Author URI:  http://www.grobator.de/
@@ -68,68 +68,94 @@ add_action('edit_link', 'faviroll_single_favicon');
 add_action('add_link' , 'faviroll_single_favicon');
 
 
-/**
- * Add option page
- */
-function faviroll_options(){
+#################
+#  Admin Area   #
+#################
+if (is_admin()) {
 
-	$message = null;
-	$removeSettings = isset($_REQUEST['faviroll_remove_settings']);
-
-	// Initialize plugin options if not remove settings is requested
-	$faviroll = new Faviroll(!$removeSettings);
-
-	$nonce = isset($_REQUEST['_wpnonce']) ? $_REQUEST['_wpnonce'] : null;
-	if (wp_verify_nonce($nonce, 'my-nonce') ) {
-
-		if($removeSettings) {
-
-			$faviroll->flush(true);
-			$faviroll->removeSettings();
-
-			$message = 'Plugin settings and cached icons removed.<br /><a href="./plugins.php?plugin_status=active">Now you can deactivate the plugin</a>';
-
-		} else {
-
-			$was_transparency = (get_option('faviroll_transparency') == 'on');
-
-			$defico = trim($_REQUEST['faviroll_default_favicon']);
-			update_option('faviroll_default_favicon', (empty($defico) ? $faviroll->getFavirollDefaultIcon() : $defico));
-
-			update_option('faviroll_revisit'       , (int) trim($_REQUEST['faviroll_revisit']));
-			update_option('faviroll_transparency'  , (isset($_REQUEST['faviroll_transparency']) ? 'on' : 'off') );
-			update_option('faviroll_debug'         , (isset($_REQUEST['faviroll_debug']) ? 'on' : 'off') );
-			update_option('faviroll_use_stylesheet', (isset($_REQUEST['faviroll_use_stylesheet']) ? 'on' : 'off') );
-
-			$message = 'Settings updated';
-		}
+	/**
+	 * Add Settings link to plugin page
+	 * @param unknown_type $links
+	 */
+	function faviroll_addConfigureLink( $links ) {
+		$settings_link = '<a href="link-manager.php?page='.basename(__FILE__).'">'. __('Settings').'</a>';
+		array_unshift( $links, $settings_link );
+		return $links;
 	}
+	$plugin = plugin_basename(__FILE__);
+	add_filter("plugin_action_links_$plugin", 'faviroll_addConfigureLink' );
 
-	// create nonce
-	$nonce = wp_create_nonce('my-nonce');
+	/**
+	 * Register Faviroll menu in general options menu
+	 */
+	function faviroll_menu() {
+		add_submenu_page('link-manager.php', __('FaviRoll', 'faviroll'), __('FaviRoll', 'faviroll'), 8, basename(__FILE__), 'faviroll_options');
+	}
+	add_action('admin_menu', 'faviroll_menu');
 
-	$default_favicon = get_option('faviroll_default_favicon');
-	$revisit = ($removeSettings) ? null : (int) get_option('faviroll_revisit');
 
-	$is_transparency = (get_option('faviroll_transparency') == 'on');
-	$is_debugMode    = (get_option('faviroll_debug') == 'on');
-	$use_stylesheet  = (get_option('faviroll_use_stylesheet') == 'on');
+	/**
+	 * Add option page
+	 */
+	function faviroll_options(){
 
-	if (!$removeSettings) {
+		$message = null;
+		$removeSettings = isset($_REQUEST['faviroll_remove_settings']);
 
-		if (isset($was_transparency) && ($is_transparency != $was_transparency)) {
-			$message = 'Transparency switched';
-			$_REQUEST['faviroll_renew_icons'] = 'true';
+		// Initialize plugin options if not remove settings is requested
+		$faviroll = new Faviroll(!$removeSettings);
+
+		$nonce = isset($_REQUEST['_wpnonce']) ? $_REQUEST['_wpnonce'] : null;
+		if (wp_verify_nonce($nonce, 'my-nonce') ) {
+
+			if($removeSettings) {
+
+				$faviroll->flush(true);
+				$faviroll->removeSettings();
+
+				$message = 'Plugin settings and cached icons removed.<br /><a href="./plugins.php?plugin_status=active">Now you can deactivate the plugin</a>';
+
+			} else {
+
+				$was_transparency = (get_option('faviroll_transparency') == 'on');
+
+				$defico = trim($_REQUEST['faviroll_default_favicon']);
+				update_option('faviroll_default_favicon', (empty($defico) ? $faviroll->getFavirollDefaultIcon() : $defico));
+
+				update_option('faviroll_revisit'       , (int) trim($_REQUEST['faviroll_revisit']));
+				update_option('faviroll_transparency'  , (isset($_REQUEST['faviroll_transparency']) ? 'on' : 'off') );
+				update_option('faviroll_debug'         , (isset($_REQUEST['faviroll_debug']) ? 'on' : 'off') );
+				update_option('faviroll_use_stylesheet', (isset($_REQUEST['faviroll_use_stylesheet']) ? 'on' : 'off') );
+
+				$message = 'Settings updated';
+			}
 		}
-		if(isset($_REQUEST['faviroll_renew_icons']))
+
+		// create nonce
+		$nonce = wp_create_nonce('my-nonce');
+
+		$default_favicon = get_option('faviroll_default_favicon');
+		$revisit = ($removeSettings) ? null : (int) get_option('faviroll_revisit');
+
+		$is_transparency = (get_option('faviroll_transparency') == 'on');
+		$is_debugMode    = (get_option('faviroll_debug') == 'on');
+		$use_stylesheet  = (get_option('faviroll_use_stylesheet') == 'on');
+
+		if (!$removeSettings) {
+
+			if (isset($was_transparency) && ($is_transparency != $was_transparency)) {
+				$message = 'Transparency switched';
+				$_REQUEST['faviroll_renew_icons'] = 'true';
+			}
+			if(isset($_REQUEST['faviroll_renew_icons']))
 			$faviroll->reset();
 
-	}
+		}
 
-	if (!is_null($message))
+		if (!is_null($message))
 		$message = '<div class="updated fade below-h2" id="message"><p>'.$message.'</p></div>';
 
-	echo '
+		echo '
  <div class="wrap">
   <div id="icon-options-general" class="icon32"><br /></div>
   <h2>'.__('FAVIcons for blogROLL', 'faviroll').' Settings</h2>'.$faviroll->get_message($message).'<br />
@@ -175,13 +201,13 @@ function faviroll_options(){
       <td><input type="checkbox" name="faviroll_remove_settings" value="true" />
      </tr>
 ';
-/*
-				<tr>
-					<td title="Write debug informations as comments into the HTML code">Debug mode:</td>
-			    <td><input type="checkbox" name="faviroll_debug" value="true" '.($is_debugMode ? ' checked="checked"' : null).' /> &nbsp; ( just for Developers, normally switch off )
-				</tr>
-*/
-	echo '     <tr>
+		/*
+		 <tr>
+		 <td title="Write debug informations as comments into the HTML code">Debug mode:</td>
+		 <td><input type="checkbox" name="faviroll_debug" value="true" '.($is_debugMode ? ' checked="checked"' : null).' /> &nbsp; ( just for Developers, normally switch off )
+		 </tr>
+		 */
+		echo '     <tr>
       <td class="submit"><input type="submit" name="submit_button" value="Submit" /><input type="hidden" name="_wpnonce" value="'.$nonce.'" /></td>
      </tr>
     </table>
@@ -189,48 +215,38 @@ function faviroll_options(){
   </div>
 ';
 
-		ob_flush();
-		flush();
-}
-
-
-/**
- * Register Faviroll menu in general options menu
- */
-function faviroll_menu() {
-	add_submenu_page('link-manager.php', __('FaviRoll', 'faviroll'), __('FaviRoll', 'faviroll'), 8, basename(__FILE__), 'faviroll_options');
-}
-add_action('admin_menu', 'faviroll_menu');
-
-
-/**
- * Register Enqueue CSS, if option is activated in Faviroll-Settings
- */
-if (get_option('faviroll_use_stylesheet') == 'on') {
-	function faviroll_enqueue_scripts() {
-		wp_enqueue_style('faviroll', WP_PLUGIN_URL.'/faviroll/style.css', false, false, 'all');
 	}
-	add_action('wp_enqueue_scripts', 'faviroll_enqueue_scripts');
-}
-
-/**
- * Actions on plugin activation
- */
-function faviroll_activate() {
-	$faviroll = new Faviroll();
-	return $faviroll->patchPlugin('wp-render-blogroll-links',true);
-}
-register_activation_hook(__FILE__, 'faviroll_activate');
 
 
-/**
- * Actions on plugin deactivation
- */
-function faviroll_deactivate() {
-	$faviroll = new Faviroll();
-	return $faviroll->patchPlugin('wp-render-blogroll-links',false);
+	/**
+	 * Register Enqueue CSS, if option is activated in Faviroll-Settings
+	 */
+	if (get_option('faviroll_use_stylesheet') == 'on') {
+		function faviroll_enqueue_scripts() {
+			wp_enqueue_style('faviroll', WP_PLUGIN_URL.'/faviroll/style.css', false, false, 'all');
+		}
+		add_action('wp_enqueue_scripts', 'faviroll_enqueue_scripts');
+	}
+
+	/**
+	 * Actions on plugin activation
+	 */
+	function faviroll_activate() {
+		$faviroll = new Faviroll();
+		return $faviroll->patchPlugin('wp-render-blogroll-links',true);
+	}
+	register_activation_hook(__FILE__, 'faviroll_activate');
+
+	/**
+	 * Actions on plugin deactivation
+	 */
+	function faviroll_deactivate() {
+		$faviroll = new Faviroll();
+		return $faviroll->patchPlugin('wp-render-blogroll-links',false);
+	}
+	register_deactivation_hook(__FILE__, 'faviroll_deactivate');
+
 }
-register_deactivation_hook(__FILE__, 'faviroll_deactivate');
 
 /* eof */
 ?>
